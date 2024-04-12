a5363a6087adc8f4d2c8afb102acdd18fc8d43a4
ComfyKoala <7231565+ComfyKoala@users.noreply.github.com>
2024-04-12 16:38:24 -0700

    Added PyPoll code and updated README

---
+++ PyPoll/main.py
@@ -0,0 +1,65 @@
+# Your task is to create a Python script that analyzes the votes and calculates each of the following values:
+# The total number of votes cast
+# A complete list of candidates who received votes
+# The percentage of votes each candidate won
+# The total number of votes each candidate won
+# The winner of the election based on popular vote
+
+# CSV FILE ASSUMPTIONS:
+# Ballot IDs are unique integers
+# County and Candidate are strings
+
+# import os for filepaths, csv
+import os
+import csv
+
+# constants for readability
+BALLOT_COL = 0
+COUNTY_COL = 1
+CANDIDATE_COL = 2
+
+# create path to csv
+csvPath = os.path.join('Resources', 'election_data.csv')
+
+# open file for reading
+with open(csvPath) as csvFile:
+    # create reader with "," delimiter
+    csvReader = csv.reader(csvFile, delimiter=',')
+    # eat the header
+    csvHeader = next(csvReader)
+
+    #initialize trackers
+    totalVotes = 0
+    candidateVotes = {}
+
+    for row in csvReader:
+        # if the key for current data row does not exist in candidateVotes, add a count of 1
+        if not candidateVotes.get(row[CANDIDATE_COL]):
+            candidateVotes[row[CANDIDATE_COL]] = 1
+        # else add 1 to the count
+        else:
+            candidateVotes[row[CANDIDATE_COL]] += 1
+        # increment total votes
+        totalVotes += 1
+
+    print("Election Results\n-------------------------")
+    print(f"Total Votes: {totalVotes}\n-------------------------")
+
+    # track winner
+    winner = ""
+    winnerPercent = 0.0
+    # for each candidate/vote pair in dictionary
+    for candidate, votes in candidateVotes.items():
+        # calculate percent of votes for candidate
+        percentTotal = (float(votes)/totalVotes) * 100
+        # print out candidate
+        print(f"{candidate}: {percentTotal:.3f}% ({votes})")
+        # if winner is empty, this is the first row. set values accordingly
+        if not winner:
+            winner = candidate
+            winnerPercent = percentTotal
+        # if current percentTotal greater than winner, set new winner
+        elif percentTotal > winnerPercent:
+            winner = candidate
+            winnerPercent = percentTotal
+    print(f"-------------------------\nWinner: {winner}\n-------------------------")
--- README.md
+++ README.md
@@ -7,3 +7,7 @@ PyBank will analyze a CSV file "Resources/budget_data.csv" which contain unique 
 - Month with greatest increase in profits (and amount)
 - Month with greatest decrease in profits (and amount)
 ## PyPoll
+PyPoll will analyze a CSV file "Resource/election_data.csv" which contains votes identified by unique Ballot IDs, Counties, and Candidates. It will output the following:
+- Total Votes
+- Candidate Votes and Percentages
+- Winner
